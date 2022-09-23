0de8b070d0e7fcd097e3ea4df351cfbee60c9cc5
Parth <108059096+parth-exe@users.noreply.github.com>
2022-09-23 18:44:37 +0530

    Update single-linked-list.c

--- single-linked-list.c
+++ single-linked-list.c
@@ -28,37 +28,75 @@ void insertion(nodez *inserted, nodez* new){
 nodez *search(nodez *head, int var){
     nodez *temp = head;
     while(temp != NULL){
-        if(temp->var == var) return temp;
+        if(temp->var == var){
+            printf("The node is present in the LL! \n");
+            return temp;
+        }
         temp = temp->next;
     }
+    printf("Node not present in LL!\n");
     return NULL;
 }
 
 int main(){
-    int x;
-    int tail;
-    int length;
-    printf("Enter length of LL: ");
-    scanf("%d", &length);
-    nodez *head;
-    nodez *temp;
-    printf("Enter the tail of the linked list: ");
-    scanf("%d", &tail);
-    temp = createNode(tail);
-    head = temp;
-    for(int i=0; i<length-1;i++){
-        printf("Enter other elements [%d]: \n", i+1);
-        scanf("%d", &x);
-        temp = createNode(x);
-        temp->next = head;
-        head = temp;
-    }
-    print_ll(head);
-    insertion(temp->next, createNode(14));
-    print_ll(head);
-    printf("%d \n", head->next->next->var);
-    printf("%d \n", head->var);
-    temp = search(head, 13);
-    printf("Found node %d \n", temp->var);
+    int x, length, tail, choice, tmp;
+    nodez *head, *temp;
+    do{
+        printf("Enter the operations on linked list: \n");
+        printf("1. Create \n");
+        printf("2. Display \n");
+        printf("3. Insert \n");
+        printf("4. Delete \n");
+        printf("5. Search \n");
+        printf("0. To exit code \n");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:
+                printf("Enter the length of linked list: \n");
+                scanf("%d", &length);
+                printf("Enter the tail element: \n");
+                scanf("%d", &tail);
+                temp = createNode(tail);
+                head = temp;
+                for(int i=0; i<length-1;i++){
+                    printf("Enter other elements [%d]: \n", i+1);
+                    scanf("%d", &x);
+                    temp = createNode(x);
+                    temp->next = head;
+                    head = temp;
+                }
+                print_ll(head);
+                break;
+            case 2:
+                print_ll(head);
+                break;
+            case 3:
+                printf("Enter the element for insertion: \n");
+                scanf("%d", &tmp);
+                insertion(temp->next, createNode(tmp));
+                print_ll(head);
+                break;
+            case 4:
+                printf("Enter element for deletion: \n");
+                scanf("%d", tmp);
+                
+                break;
+            case 5:
+                printf("Enter the node to be searched: \n");
+                scanf("%d", &tmp);
+                temp = search(head, tmp);
+
+
+            default:
+                printf("Enter choice again! \n");
+                break;
+
+        }
+        if(choice == 0){
+            break;
+        }
+    }while(1);
+
     return 0;
 }
